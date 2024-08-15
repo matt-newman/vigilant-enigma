601636d762e6aa9dd139cf9cd5cc9b9ed5cc9f3d
Matt Newman <matthew.newman@cloudcall.com>
2024-08-15 12:00:35 +0100

    shell of table structure

--- org/apps/rooser/src/app/components/table/Table.tsx
+++ org/apps/rooser/src/app/components/table/Table.tsx
@@ -7,9 +7,42 @@ type tableProps = {
 
 export const Table = ({ headerText, data }:tableProps) => {
     return (
-        <div className="table-wrapper">
-            <h2 className="table-header">{headerText}</h2>
+        <div className="table-wrapper rounded shadow">
+            <h2 className="table-header collapsible open">{headerText}</h2>
             <table className="table">
+                <thead>
+                    <tr>
+                        <th className='hidden-text'>Item</th>
+                        <th>Quality</th>
+                        <th>Dispatch Date</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th className='hidden-text'>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            Item
+                        </td>
+                        <td>
+                            A+
+                        </td>
+                        <td>
+                            Today
+                        </td>
+                        <td>
+                            60 x 5 kg
+                        </td>
+                        <td>
+                            €8.65
+                        </td>
+                        <td>
+                            <button>Accept</button>
+                            <button>Ignore</button>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
